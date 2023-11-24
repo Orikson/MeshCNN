9dec51bfccdb3a1b048dd3ef871eab3255f3b502
Orikson <42471346+Orikson@users.noreply.github.com>
2023-11-23 17:07:41 -0800

    Remove pooling layers

--- models/networks.py
+++ models/networks.py
@@ -134,10 +134,10 @@ class MeshConvNet(nn.Module):
         for i, ki in enumerate(self.k[:-1]):
             setattr(self, 'conv{}'.format(i), MResConv(ki, self.k[i + 1], nresblocks))
             setattr(self, 'norm{}'.format(i), norm_layer(**norm_args[i]))
-            setattr(self, 'pool{}'.format(i), MeshPool(self.res[i + 1]))
+            #setattr(self, 'pool{}'.format(i), MeshPool(self.res[i + 1]))
 
 
-        self.gp = torch.nn.AvgPool1d(self.res[-1])
+        #self.gp = torch.nn.AvgPool1d(self.res[-1])
         # self.gp = torch.nn.MaxPool1d(self.res[-1])
         self.fc1 = nn.Linear(self.k[-1], fc_n)
         self.fc2 = nn.Linear(fc_n, nclasses)
@@ -147,9 +147,9 @@ class MeshConvNet(nn.Module):
         for i in range(len(self.k) - 1):
             x = getattr(self, 'conv{}'.format(i))(x, mesh)
             x = F.relu(getattr(self, 'norm{}'.format(i))(x))
-            x = getattr(self, 'pool{}'.format(i))(x, mesh)
+            #x = getattr(self, 'pool{}'.format(i))(x, mesh)
 
-        x = self.gp(x)
+        #x = self.gp(x)
         x = x.view(-1, self.k[-1])
 
         x = F.relu(self.fc1(x))
